39df5545c41e2a5e7f2b751f76964a6a2bd0b203
Arshia-AJ-10 <arshiaaj132@gmail.com>
2026-09-17 11:08:50 +0330

    Transaction and Appointment Det API done

--- app/Http/Controllers/API/AppointmentController.php
+++ app/Http/Controllers/API/AppointmentController.php
@@ -6,12 +6,37 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\Appointment;
+use Hekmatinasser\Verta\Verta;
 
 class AppointmentController extends Controller
 {
-    public function det($id) {
+    public function det($id)
+    {
         $user = User::find(6);
-        $appointment = Appointment::where('id', $id)->where('user_id',$user->id)->with('transaction')->firstOrFail();
+
+        $appointment = Appointment::where('id', $id)
+            ->where('user_id', $user->id)
+            ->with('transaction', 'assignedStaff', 'service')
+            ->firstOrFail();
+
+        $doctor = $appointment->assignedStaff;
+
+        $doctorService = $doctor
+            ? $doctor->services()->inRandomOrder()->first()
+            : null;
+
+        if ($appointment->assignedStaff) {
+            $appointment->assignedStaff['ability'] = $doctorService->name;
+        }
+
+        $appointment->appointment_date_fa = Verta::instance(
+            $appointment->getRawOriginal('appointment_date')
+        )->format('Y F d, l');
+
+        $appointment->appointment_time_fa = Verta::instance(
+            $appointment->getRawOriginal('appointment_time')
+        )->format('H:i');
+
         return response()->json([
             'success' => true,
             'message' => 'اطلاعات نوبت با موفقیت دریافت شد.',
